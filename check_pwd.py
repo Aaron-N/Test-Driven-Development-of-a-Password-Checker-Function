ff41e28ba953be10e2abb16ecd2106c66470b38b
Aaron N <Aaron-N@users.noreply.github.com>
2021-02-13 17:30:29 -0800

    Added min password length of 8 check, test1 now passes

--- check_pwd.py
+++ check_pwd.py
@@ -1,7 +1,11 @@
 # Author:  Aaron Nesbit
-# Date:    2/12/2021
+# Date:    2/13/2021
 # Course:  CS362-400-W2021
 # Program: Activity 2 - TDD
 
 def check_pwd(password):
+    # Check if password meets minimum length of 8 requirement
+    if len(password) < 8:
+        return False
+    
     return True
