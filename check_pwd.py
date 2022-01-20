ae9969020e108438aa610c12206c97c30006ffde
Aaron N <Aaron-N@users.noreply.github.com>
2022-01-19 18:43:47 -0800

    Update check_pwd.py

--- check_pwd.py
+++ check_pwd.py
@@ -1,7 +1,5 @@
 # Author:  Aaron Nesbit
 # Date:    2/13/2021
-# Course:  CS362-400-W2021
-# Program: Activity 2 - TDD
 
 def check_pwd(password):
     # Check if password meets minimum length of 8 requirement
